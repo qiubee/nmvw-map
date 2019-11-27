be77444a476b5342938052cf86fc7e4e3ba28198
qiubee <45637654+qiubee@users.noreply.github.com>
2019-11-27 10:50:29 +0100

    update website

--- js/index.js
+++ js/index.js
@@ -32,21 +32,46 @@ var nmvw = {
     long: 128.5
   }]
 };
-
 var projection = d3.geoPatterson();
-var path = d3.geoPath().projection(projection); 
+var path = d3.geoPath().projection(projection);
+var scale = d3.scaleSqrt();
 
 // -- Elementen aanmaken --
 var title = d3.select("div").append("h2").text("Wereldkaart met populaire categorieën per continent en per land.");
 var explanation = d3.select("div").append("p").text("Klik op een cirkel om in te zoomen en selecteer een land om dieper in de collectie te duiken.");
 var svg = d3.select("div").append("svg").attr("viewBox", "50 0 850 496");
 var legend = d3.select("div").append("h3").text("Legenda");
-var legendContent = d3.select("div").append("p"); 
+var legendContent = d3.select("div").append("p");
+
+visualize(); 
 
 // --- Visualiseren ---
-deleteNoScript();
-drawMap();
-configureData(nmvw.apiURL, nmvw.apiQuery);
+function visualize() {
+  var draw, data;
+  return regeneratorRuntime.async(function visualize$(_context) {
+    while (1) {
+      switch (_context.prev = _context.next) {
+        case 0:
+          deleteNoScript();
+          _context.next = 3;
+          return regeneratorRuntime.awrap(drawMap());
+
+        case 3:
+          draw = _context.sent;
+          _context.next = 6;
+          return regeneratorRuntime.awrap(configureData(nmvw.apiURL, nmvw.apiQuery));
+
+        case 6:
+          data = _context.sent;
+          plotData(data);
+
+        case 8:
+        case "end":
+          return _context.stop();
+      }
+    }
+  });
+}
 
 // Verwijder noscript
 function deleteNoScript() {
@@ -54,46 +79,97 @@ function deleteNoScript() {
   d3.select("div").attr("class", null);
 }
 
-// Wereldkaart maken met d3
+// Wereldkaart maken met d3 
+// kaart maken met world-atlas (voorbeeld van: https://www.youtube.com/watch?v=Qw6uAg3EO64)
 function drawMap() {
-  // kaart maken met world-atlas (voorbeeld van: https://www.youtube.com/watch?v=Qw6uAg3EO64)
-  d3.json("https://unpkg.com/world-atlas@1.1.4/world/110m.json").then(function (data) {
-    var countries = topojson.feature(data, data.objects.countries);
-    svg.selectAll("path").data(countries.features).enter().append("path").attr("class", "country").attr("d", function (d) {
-      return path(d);
-    });
+  var data, countries;
+  return regeneratorRuntime.async(function drawMap$(_context2) {
+    while (1) {
+      switch (_context2.prev = _context2.next) {
+        case 0:
+          _context2.next = 2;
+          return regeneratorRuntime.awrap(d3.json("https://unpkg.com/world-atlas@1.1.4/world/110m.json"));
+
+        case 2:
+          data = _context2.sent;
+          countries = topojson.feature(data, data.objects.countries);
+          svg.append("g").attr("class", "countries").selectAll("path").data(countries.features).enter().append("path").attr("d", function (d) {
+            return path(d);
+          });
+
+        case 5:
+        case "end":
+          return _context2.stop();
+      }
+    }
   });
 }
 
 // Data op de wereldkaart zetten
 // code gebruikt van: https://stackoverflow.com/questions/21397608/put-markers-to-a-map-generated-with-tocsript en https://stackoverflow.com/questions/26956778/plotting-points-on-a-map-with-d3-js)
 function plotData(data) {
-  svg.selectAll("circle").data(data).enter().append("circle").attr("class", "continent").attr("cx", function (d) {
+  
+  // enter data
+  svg.append("g").attr("class", "continents").selectAll("circle").data(data).enter().append("circle").attr("cx", function (d) {
     return projection([d.long, d.lat])[0];
   }).attr("cy", function (d) {
     return projection([d.long, d.lat])[1];
-  }).attr("r", "1em");
+  }).transition().duration(1500).attr("r", function (d) {
+    return scale(d.objects) / 10;
+  }); 
+  
+  // geef naam en objecten continent (transitie naam naar boven, objecten naar beneden)
+  // text op cirkel zetten http://thenewcode.com/482/Placing-Text-on-a-Circle-with-SVG
+  svg.selectAll("circle").on("mouseover", function () {
+    d3.select(this).append("text").text(function (d) {
+      return d.key;
+    });
+    d3.select(this).attr("r", function (d) {
+      return scale(d.objects) / 10;
+    }).transition().attr("r", function (d) {
+      return scale(d.objects) / 10 - scale(d.objects) / 120;
+    });
+  }).on("mouseout", function () {
+    d3.select(this).attr("r", function (d) {
+      return scale(d.objects) / 10 - scale(d.objects) / 120;
+    }).transition().attr("r", function (d) {
+      return scale(d.objects) / 10;
+    }).text(null);
+  });
+  
+  // make bubble chart (voorbeeld gebruikt van: https://observablehq.com/@d3/zoom-to-bounding-box, 
+  // https://observablehq.com/@rocss/test en https://observablehq.com/@mbostock/clustered-bubbles
+  svg.selectAll("circle").on("click", function () {
+    console.log(this); // let continent;
+    // for (let id of data) {
+    //     if (id.key === this.__data__.key) {
+    //         continent = id.values;
+    //     }
+    // }
+    // console.log(continent);
+  });
 }
 
 // -- Data ophalen en verwerken --
 function configureData(url, query) {
   var data;
-  return regeneratorRuntime.async(function configureData$(_context) {
+  return regeneratorRuntime.async(function configureData$(_context3) {
     while (1) {
-      switch (_context.prev = _context.next) {
+      switch (_context3.prev = _context3.next) {
         case 0:
-          _context.next = 2;
+          _context3.next = 2;
           return regeneratorRuntime.awrap(getData(url, query));
 
         case 2:
-          data = _context.sent;
-          data = transformData(data);
-          console.log("Transformed data: ", data);
-          plotData(data);
+          data = _context3.sent;
+          // console.log("Raw data: ", data);
+          data = transformData(data); //console.log("Transformed data: ", data);
 
-        case 6:
+          return _context3.abrupt("return", data);
+
+        case 5:
         case "end":
-          return _context.stop();
+          return _context3.stop();
       }
     }
   });
@@ -102,78 +178,84 @@ function configureData(url, query) {
 // Data ophalen
 function getData(url, query) {
   var response, json, data;
-  return regeneratorRuntime.async(function getData$(_context2) {
+  return regeneratorRuntime.async(function getData$(_context4) {
     while (1) {
-      switch (_context2.prev = _context2.next) {
+      switch (_context4.prev = _context4.next) {
         case 0:
-          _context2.next = 2;
+          _context4.next = 2;
           return regeneratorRuntime.awrap(fetch(url + "?query=" + encodeURIComponent(query) + "&format=json"));
 
         case 2:
-          response = _context2.sent;
-          _context2.next = 5;
+          response = _context4.sent;
+          _context4.next = 5;
           return regeneratorRuntime.awrap(response.json());
 
         case 5:
-          json = _context2.sent;
+          json = _context4.sent;
           data = json.results.bindings;
-          return _context2.abrupt("return", data);
+          return _context4.abrupt("return", data);
 
         case 8:
         case "end":
-          return _context2.stop();
+          return _context4.stop();
       }
     }
   });
-} 
+}
 
 // Data transformeren
 function transformData(data) {
   data = filterData(data);
+  // console.log("Filtered data: ", data);
+  // data = translateCountryToDutch(data);
+  // console.log("Translated country data: ", data);
   data = groupData(data);
+  // console.log("Grouped data: ", data);
   data = addContinentLatLong(data);
-  data = calculateData(data); 
+  // console.log("Add coordinates to continents: ", data);
+  data = calculateData(data);
+  // console.log("Calculated data: ", data);
   return data;
 }
 
 // Data filteren
 function filterData(data) {
   data = data.map(function (item) {
-    var newArr = {};
+    var filtered = {};
 
     if (item.hasOwnProperty("placeName") === true) {
-      newArr.place = item.placeName.value;
+      filtered.place = item.placeName.value;
     }
 
     if (item.hasOwnProperty("continent") === true) {
-      newArr.continent = item.continent.value;
+      filtered.continent = item.continent.value;
     }
 
     if (item.hasOwnProperty("countryName") === true) {
-      newArr.country = item.countryName.value;
+      filtered.country = item.countryName.value;
     }
 
     if (item.hasOwnProperty("lat") === true) {
-      newArr.lat = Number(item.lat.value);
+      filtered.lat = Number(item.lat.value);
     }
 
     if (item.hasOwnProperty("long") === true) {
-      newArr.long = Number(item.long.value);
+      filtered.long = Number(item.long.value);
     }
 
     if (item.hasOwnProperty("category") === true) {
-      newArr.category = item.category.value;
+      filtered.category = item.category.value;
     }
 
     if (item.hasOwnProperty("objCount") === true) {
-      newArr.objects = Number(item.objCount.value);
+      filtered.objects = Number(item.objCount.value);
     }
 
     if (item.hasOwnProperty("type") === true) {
-      newArr.type = item.type.value;
+      filtered.type = item.type.value;
     }
 
-    return newArr;
+    return filtered;
   });
   return data;
 }
@@ -186,7 +268,7 @@ function groupData(data) {
     return d.country;
   }).entries(data);
   return data;
-}
+} 
 
 // Voeg coordinaten van continenten toe
 function addContinentLatLong(data) {
@@ -224,13 +306,14 @@ function addContinentLatLong(data) {
 
 // Maak berekeningen met data
 function calculateData(data) {
+  
   // tel alle landen van continent
   data.forEach(function (continent) {
     continent.countries = continent.values.length;
     return continent.countries;
   });
   
-  // tel alle objecten
+  // telt alle objecten
   var amount = 0;
   var _iteratorNormalCompletion2 = true;
   var _didIteratorError2 = false;
@@ -239,52 +322,56 @@ function calculateData(data) {
   try {
     for (var _iterator2 = data[Symbol.iterator](), _step2; !(_iteratorNormalCompletion2 = (_step2 = _iterator2.next()).done); _iteratorNormalCompletion2 = true) {
       var continent = _step2.value;
-      var _iteratorNormalCompletion5 = true;
-      var _didIteratorError5 = false;
-      var _iteratorError5 = undefined;
+      var _iteratorNormalCompletion7 = true;
+      var _didIteratorError7 = false;
+      var _iteratorError7 = undefined;
 
       try {
-        for (var _iterator5 = continent.values[Symbol.iterator](), _step5; !(_iteratorNormalCompletion5 = (_step5 = _iterator5.next()).done); _iteratorNormalCompletion5 = true) {
-          var country = _step5.value;
-          var _iteratorNormalCompletion6 = true;
-          var _didIteratorError6 = false;
-          var _iteratorError6 = undefined;
+        for (var _iterator7 = continent.values[Symbol.iterator](), _step7; !(_iteratorNormalCompletion7 = (_step7 = _iterator7.next()).done); _iteratorNormalCompletion7 = true) {
+          var country = _step7.value;
+          var _iteratorNormalCompletion8 = true;
+          var _didIteratorError8 = false;
+          var _iteratorError8 = undefined;
 
           try {
-            for (var _iterator6 = country.values[Symbol.iterator](), _step6; !(_iteratorNormalCompletion6 = (_step6 = _iterator6.next()).done); _iteratorNormalCompletion6 = true) {
-              var category = _step6.value;
+            for (var _iterator8 = country.values[Symbol.iterator](), _step8; !(_iteratorNormalCompletion8 = (_step8 = _iterator8.next()).done); _iteratorNormalCompletion8 = true) {
+              var category = _step8.value;
               amount += category.objects;
             }
           } catch (err) {
-            _didIteratorError6 = true;
-            _iteratorError6 = err;
+            _didIteratorError8 = true;
+            _iteratorError8 = err;
           } finally {
             try {
-              if (!_iteratorNormalCompletion6 && _iterator6.return != null) {
-                _iterator6.return();
+              if (!_iteratorNormalCompletion8 && _iterator8.return != null) {
+                _iterator8.return();
               }
             } finally {
-              if (_didIteratorError6) {
-                throw _iteratorError6;
+              if (_didIteratorError8) {
+                throw _iteratorError8;
               }
             }
           }
         }
       } catch (err) {
-        _didIteratorError5 = true;
-        _iteratorError5 = err;
+        _didIteratorError7 = true;
+        _iteratorError7 = err;
       } finally {
         try {
-          if (!_iteratorNormalCompletion5 && _iterator5.return != null) {
-            _iterator5.return();
+          if (!_iteratorNormalCompletion7 && _iterator7.return != null) {
+            _iterator7.return();
           }
         } finally {
-          if (_didIteratorError5) {
-            throw _iteratorError5;
+          if (_didIteratorError7) {
+            throw _iteratorError7;
           }
         }
       }
     }
+    
+    // console.log("Total objects: ", amount);
+    
+    // tel alle objecten van continent ---> Tip danny: loop over de key en values van het object
   } catch (err) {
     _didIteratorError2 = true;
     _iteratorError2 = err;
@@ -300,9 +387,6 @@ function calculateData(data) {
     }
   }
 
-  console.log("Total objects: ", amount); 
-  
-  // tel alle objecten van continent
   data.forEach(function (continent) {
     var objects = 0;
     var _iteratorNormalCompletion3 = true;
@@ -350,7 +434,6 @@ function calculateData(data) {
         }
       }
     }
-
     continent.objects = objects;
     return continent.amount;
   });
